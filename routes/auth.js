ca4bb6a4a9c17c8216dec0eb6fb6bb471e2b49b7
code-X-AJ <amit24032005@gmail.com>
2023-02-19 18:19:34 +0530

    Story app created

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+// @desc Auth with Google
+// @route GET/auth/google
+router.get('/google',passport.authenticate('google',{scope: ['profile']}))
+
+// @desc google auth callback
+// @route GET/auth/google/callback
+router.get('/google/callback', passport.authenticate('google',{failureRedirect:'/'}),(req,res)=>{
+    res.redirect('/dashboard')
+})
+
+// @desc    Logout user
+// @route   /auth/Logout
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+  });
+  
+
+module.exports = router
